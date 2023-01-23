02c4e19949a84bd8d93f663d9eccf92df38739ad
wangsj <wangshaojiex@163.com>
2023-01-23 12:44:13 +0800

    key and mouse control

--- get_started.cpp
+++ get_started.cpp
@@ -3,26 +3,32 @@
 #include "GLFW/glfw3.h"
 #include "Shader.h"
 
+#include "glm/glm.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-//const char* vertexShader = "#version 330 core \n"
-//"layout(location=0) in vec3 aPos;\n"
-//"void main()\n"
-//"{\n"
-//"gl_Position = vec4(aPos.xyz,1.0f);\n"
-//"}\0";
-//
-//const char* fragmentShader = "#version 330 core\n"
-//"out vec4 FragColor;\n"
-//"uniform vec4 OurColor;\n"
-//"void main()\n"
-//"{\n"
-//"FragColor = OurColor;\n"
-//"}\0";
+#define SCR_WIDTH 800
+#define SCR_HEIGHT 600
+
+// camera
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+float fov = 45.0f;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xPos, double yPos);
+void scroll_callback(GLFWwindow* window, double xOffset, double yOffset);
 void processInput(GLFWwindow* window);
+
+//====================================================================================//
 int main()
 {
 	std::cout << "get started" << std::endl;
@@ -43,6 +49,11 @@ int main()
 
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// capture mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
@@ -53,57 +64,60 @@ int main()
 		std::cout << "glew init error" << std::endl;
 		return -1;
 	}
-
-	////shader
-	//unsigned int vs = glCreateShader(GL_VERTEX_SHADER);
-	//glShaderSource(vs, 1, &vertexShader, NULL);
-	//glCompileShader(vs);
-
-	//// check for shader compile errors
-	//int success;
-	//char infoLog[512];
-	//glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
-	//if (!success)
-	//{
-	//	glGetShaderInfoLog(vs, 512, NULL, infoLog);
-	//	std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	//}
-
-	//unsigned int fs = glCreateShader(GL_FRAGMENT_SHADER);
-	//glShaderSource(fs, 1, &fragmentShader, NULL);
-	//glCompileShader(fs);
-
-	//glGetShaderiv(fs, GL_COMPILE_STATUS, &success);
-	//if (!success)
-	//{
-	//	glGetShaderInfoLog(fs, 512, NULL, infoLog);
-	//	std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	//}
-
-	//unsigned int shaderProgram = glCreateProgram();
-	//glAttachShader(shaderProgram, vs);
-	//glAttachShader(shaderProgram, fs);
-	//glLinkProgram(shaderProgram);
-
-	//// check for linking errors
-	//glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	//if (!success) {
-	//	glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-	//	std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-	//}
-	//glDeleteShader(vs);
-	//glDeleteShader(fs);
-
-	//unsigned int colorLocation = glGetUniformLocation(shaderProgram, "OurColor");
 	
+	// shader
 	Shader shader("res/shaders/simplevertex.vs", "res/shaders/simplefragment.frag");
 
 	//
-	float verticles[] = {
+	/*float vertices[] = {
 		-0.5f, -0.5f, 0.0f, 0, 0,
 		 0.5f, -0.5f, 0.0f, 2, 0,
 		 0.5f, 0.5f, 0.0f, 2, 2,
 		-0.5f, 0.5f, 0.0f, 0, 2
+	};*/
+	// ------------------------------------------------------------------
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
 
 	unsigned int indices[] = {
@@ -111,10 +125,24 @@ int main()
 		0, 2, 3
 	};
 
+	// world space positions of our cubes
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
 	unsigned int vbo;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);//bind vbo
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verticles), verticles, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -137,7 +165,6 @@ int main()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-
 	int width, height, nrChannel;
 	unsigned char* data = stbi_load("res/textures/container.jpg", &width, &height, &nrChannel, 0);
 	if (data)
@@ -145,6 +172,7 @@ int main()
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
+	stbi_image_free(data);
 
 	unsigned int texture2;
 	glGenTextures(1, &texture2);
@@ -161,23 +189,35 @@ int main()
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width2, height2, 0, GL_RGBA, GL_UNSIGNED_BYTE, data2);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
+	stbi_image_free(data2);
 
 	shader.use();
 	glUniform1i(glGetUniformLocation(shader.getId(), "texContainer"), 0);
 	glUniform1i(glGetUniformLocation(shader.getId(), "texAwesomeface"), 1);
 
+	glm::mat4 model = glm::mat4(1.0f); // init identity mat
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 project = glm::mat4(1.0f);
+	model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 0, 0));
+	view = glm::lookAt(glm::vec3(0, 3.0f, 3.0f), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glUniformMatrix4fv(glGetUniformLocation(shader.getId(), "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+	//
+	glEnable(GL_DEPTH_TEST);
 	while (!glfwWindowShouldClose(window))
 	{
+		// pre calculate
+		double curTime = glfwGetTime();
+		deltaTime = curTime - lastFrame;
+		lastFrame = curTime;
+
 		//process user input
 		processInput(window);
 
 		//clear back color
 		glClearColor(0.5f, 0.5f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
-		/*glUseProgram(shaderProgram);
-		glUniform4f(colorLocation, 1.0f, 1.0f, .0f, 0.5f);*/
 		shader.use();
 
 		glActiveTexture(GL_TEXTURE0);
@@ -185,8 +225,32 @@ int main()
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture2);
 
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		/*float radius = 10.0f;
+		float camX = static_cast<float>(cos(glfwGetTime()) * radius);
+		float camZ = static_cast<float>(sin(glfwGetTime()) * radius);
+		view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));*/
+
+		// camera/view transformation
+		glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+		glUniformMatrix4fv(glGetUniformLocation(shader.getId(), "view"), 1, GL_FALSE, glm::value_ptr(view));
+
+		project = glm::perspective(glm::radians(fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		glUniformMatrix4fv(glGetUniformLocation(shader.getId(), "projection"), 1, GL_FALSE, glm::value_ptr(project));
+
+		//glDrawArrays(GL_TRIANGLES, 0, 36);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			// calculate the model matrix for each object and pass it to shader before drawing
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			glUniformMatrix4fv(glGetUniformLocation(shader.getId(), "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -206,10 +270,80 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
+glm::vec2 lastPos = glm::vec2(0.0f, 0.0f);
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+bool firstMouse = true;
+void mouse_callback(GLFWwindow* window, double xPos, double yPos)
+{
+	if (window == NULL)
+	{
+		return;
+	}
+
+	if (firstMouse)
+	{
+		firstMouse = false;
+		lastPos.x = xPos;
+		lastPos.y = yPos;
+	}
+
+	float deltaX = xPos - lastPos.x;
+	float deltaY = yPos - lastPos.y;
+	lastPos.x = xPos;
+	lastPos.y = yPos;
+
+	float sensitivity = 0.1f; // change this value to your liking
+	deltaX *= sensitivity;
+	deltaY *= sensitivity;
+
+	yaw += deltaX;
+	pitch -= deltaY;
+
+	// make sure that when pitch is out of bounds, screen doesn't get flipped
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(front);
+}
+
+void scroll_callback(GLFWwindow* window, double xOffset, double yOffset)
+{
+	fov -= (float)yOffset;
+	if (fov < 1.0f)
+		fov = 1.0f;
+	if (fov > 60.0f)
+		fov = 60.0f;
+}
+
 void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
 		glfwSetWindowShouldClose(window, true);
+	}
+
+	float cameraSpeed = static_cast<float>(2.5 * deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		cameraPos -= cameraSpeed * glm::normalize((glm::cross(cameraFront, cameraUp)));
+	}
+	else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		cameraPos += cameraSpeed * glm::normalize((glm::cross(cameraFront, cameraUp)));
+	}
+	else if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		cameraPos -= cameraSpeed * cameraFront;
+	}
+	else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		cameraPos += cameraSpeed * cameraFront;
 	}
 }
--- res/shaders/simplevertex.vs
+++ res/shaders/simplevertex.vs
@@ -2,11 +2,15 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTexcoord;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 
 out vec2 texCoord;
 
 void main()
 {
 	texCoord = aTexcoord.xy;
-	gl_Position = vec4(aPos, 1.0f);
+	gl_Position = projection * view * model * vec4(aPos, 1.0f);
 }
